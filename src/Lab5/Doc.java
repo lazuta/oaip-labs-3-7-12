0b657d5e2066c333b3205a337e04765a25f335f4
Lazuta <lenya.lazuta@mail.ru>
2020-01-10 00:52:46 +0300

    lab 5

---
+++ src/Lab5/Doc.java
@@ -0,0 +1,53 @@
+package Lab5;
+
+public class Doc extends Plant {
+    public int maxShip;
+    public double speedRepair;
+
+    public Doc() {}
+
+    public Doc(int countShip, int maxShip, int speedRepair) {
+        this.countShip = countShip;
+        this.maxShip = maxShip;
+        this.speedRepair = speedRepair;
+    }
+
+    public Doc(int maxPeople, double size, double power, String street, int countShip, int maxShip, double speedRepair) {
+        super(maxPeople, size, power, street, countShip);
+        this.maxShip = maxShip;
+        this.speedRepair = speedRepair;
+    }
+
+    public int getMaxShip() {
+        return maxShip;
+    }
+
+    public void setMaxShip(int maxShip) {
+        this.maxShip = maxShip;
+    }
+
+    public double getSpeedRepair() {
+        return speedRepair;
+    }
+
+    public void setSpeedRepair(int speedRepair) {
+        this.speedRepair = speedRepair;
+    }
+
+    public double timeRepair() {
+        return (countShip / speedRepair) * (super.power * 0.1);
+    }
+
+    public void repair() {
+        super.done();
+    }
+
+    @Override
+    public String toString() {
+        return "Doc{" +
+                "countShip=" + countShip +
+                ", maxShip=" + maxShip +
+                ", speedRepair=" + speedRepair +
+                '}';
+    }
+}
---
+++ src/Lab5/Plant.java
@@ -0,0 +1,81 @@
+package Lab5;
+
+public class Plant {
+    protected int maxPeople;
+    protected double size;
+    protected double power;
+    protected String street;
+    protected int countShip;
+
+    public Plant() {}
+
+    public Plant(int maxPeople, double size, double power, String street, int countShip) {
+        this.maxPeople = maxPeople;
+        this.size = size;
+        this.power = power;
+        this.street = street;
+        this.countShip = countShip;
+    }
+
+    public int getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(int maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getCountShip() {
+        return countShip;
+    }
+
+    public void setCountShip(int countShip) {
+        this.countShip = countShip;
+    }
+
+    protected void income() {
+        System.out.println((power / maxPeople) > 1 ? ("Завод (" + street + ") функционирует прекрасно.") : ("Завод (" + street + ") несет финансовые поетри"));
+    }
+
+    protected String done() {
+        return "Единица продукции произведена";
+    }
+
+    protected double timeDone() {
+        return size / power;
+    }
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "maxPeople=" + maxPeople +
+                ", size=" + size +
+                ", power=" + power +
+                ", street='" + street + '\'' +
+                '}';
+    }
+}
