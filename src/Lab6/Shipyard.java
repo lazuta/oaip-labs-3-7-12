da4639b3ea93dd3619d393acb76e5260231d8321
Lazuta <lenya.lazuta@mail.ru>
2020-01-10 01:00:08 +0300

    lab 6, lab 7

---
+++ src/Lab6/Shipyard.java
@@ -0,0 +1,80 @@
+package Lab6;
+
+public class Shipyard extends Plant {
+    public int maxShip;
+    public double length;
+    public double lengthShip;
+
+    public Shipyard() {}
+
+    public Shipyard(int maxShip, double length, double lengthShip) {
+        this.maxShip = maxShip;
+        this.length = length;
+        this.lengthShip = lengthShip;
+    }
+
+    public Shipyard(int maxPeople, double size, double power, String street, int countShip, int maxShip, double length, double lengthShip) {
+        super(maxPeople, size, power, street, countShip);
+        this.maxShip = maxShip;
+        this.length = length;
+        this.lengthShip = lengthShip;
+    }
+
+    public int getMaxShip() {
+        return maxShip;
+    }
+
+    public void setMaxShip(int maxShip) {
+        this.maxShip = maxShip;
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLengthShip() {
+        return lengthShip;
+    }
+
+    public void setLengthShip(double lengthShip) {
+        this.lengthShip = lengthShip;
+    }
+
+    public int freeCount() {
+        return maxShip - super.countShip;
+    }
+
+    public double freeLength() {
+        return length - lengthShip;
+    }
+
+    public void add() {
+        if(freeCount() > 0) {
+            System.out.println("Имеются дотупные места. Заплыть можно;");
+        } else {
+            System.out.println("Отсутсвуют свободные места");
+        }
+    }
+
+    public boolean add(int count) {
+        if(freeCount() > count)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipyard{" +
+                "maxShip=" + maxShip +
+                ", countShip=" + countShip +
+                ", length=" + length +
+                ", lengthShip=" + lengthShip +
+                '}';
+    }
+}
---
+++ src/Lab7/Shipyard.java
@@ -0,0 +1,100 @@
+package Lab7;
+
+public class Shipyard extends Plant {
+    public int maxShip;
+    public double length;
+    public double lengthShip;
+
+    public Shipyard() {}
+
+    public Shipyard(int maxShip, double length, double lengthShip) {
+        this.maxShip = maxShip;
+        this.length = length;
+        this.lengthShip = lengthShip;
+    }
+
+    public Shipyard(int maxPeople, double size, double power, String street, int countShip, int maxShip, double length, double lengthShip) {
+        super(maxPeople, size, power, street, countShip);
+        this.maxShip = maxShip;
+        this.length = length;
+        this.lengthShip = lengthShip;
+    }
+
+    public int getMaxShip() {
+        return maxShip;
+    }
+
+    public void setMaxShip(int maxShip) {
+        this.maxShip = maxShip;
+    }
+
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLengthShip() {
+        return lengthShip;
+    }
+
+    public void setLengthShip(double lengthShip) {
+        this.lengthShip = lengthShip;
+    }
+
+    @Override
+    protected void math2() {
+        super.math2();
+    }
+
+    @Override
+    protected void income() {
+        super.income();
+    }
+
+    @Override
+    protected String done() {
+        return "Продукция готова. Можно отплывать.";
+    }
+
+    @Override
+    protected double timeDone() {
+        return super.timeDone();
+    }
+
+    public int freeCount() {
+        return maxShip - super.countShip;
+    }
+
+    public double freeLength() {
+        return length - lengthShip;
+    }
+
+    public void add() {
+        if(freeCount() > 0) {
+            System.out.println("Имеются дотупные места. Заплыть можно;");
+        } else {
+            System.out.println("Отсутсвуют свободные места");
+        }
+    }
+
+    public boolean add(int count) {
+        if(freeCount() > count)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipyard{" +
+                "maxShip=" + maxShip +
+                ", countShip=" + countShip +
+                ", length=" + length +
+                ", lengthShip=" + lengthShip +
+                '}';
+    }
+}
